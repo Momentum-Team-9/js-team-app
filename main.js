e0203fd59eb25ad1d4358032361786b64c81247d
sarah-wines <sarah.lodico.wines@gmail.com>
2021-07-27 10:12:09 -0400

    planning

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-const form = document.querySelector('.movie-form')
-
-
+const form = document.querySelector('.movie-form');
+const url = 'http://localhost:3000/movies';
+//Activates search bar
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    
+  event.preventDefault();
+  //add CRUD function
+  form.reset();
+});
 
-})
+//function for creating movie boxes
+
+//eventlistener function to mark movies as watched
+
+//eventtlistener to remove movies (delete)
+
+//CRUD function to pull movies
+
+function listMovies() {
+  fetch(url);
+}
