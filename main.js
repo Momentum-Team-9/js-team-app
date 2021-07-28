c6dc9404b9a5e1e03ec52f6aad01390a9ab5734f
Em <amberem@gmail.com>
2021-07-28 11:30:59 -0400

    Started PUT function to update watched time.

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-const form = document.querySelector('.movie-form');
-const url = 'http://localhost:3000/movies';
-const movieBox = document.querySelector('#movie-list');
-const movieInput = document.querySelector('#movie-input');
+const form = document.querySelector(".movie-form");
+const url = "http://localhost:3000/movies";
+const movieBox = document.querySelector("#movie-list");
+const movieInput = document.querySelector("#movie-input");
 
 listMovies();
 
 //Activates search bar
 
-form.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  //NEED TO FINISH THIS
   const movieText = movieInput.value;
-  console.log(movieText, 'movieText');
+  console.log(movieText, "movieText");
   addMovie(movieText);
   form.reset();
 });
 
 //event listener to trigger delete and put functions
-movieBox.addEventListener('click', function (event) {
-  if (event.target.classList.contains('remove')) {
-    deleteMovie(event.target)
+movieBox.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove")) {
+    deleteMovie();
   }
-})
+});
 
 function renderMovieText(movieItem, movieObj) {
   movieItem.innerHTML = `<div class="card"><span class="movie-title">${movieObj.title}</span><button class="watched">Mark as watched</button><button class="remove dangerous">Remove from list</button></div>`;
@@ -29,9 +28,9 @@ function renderMovieText(movieItem, movieObj) {
 
 //function for creating movie boxes
 function createMovieBox(movieObj) {
-  const movieItem = document.createElement('div');
+  const movieItem = document.createElement("div");
   movieItem.id = movieObj.id;
-  movieItem.classList.add('movie-card');
+  movieItem.classList.add("movie-card");
   renderMovieText(movieItem, movieObj);
   movieBox.appendChild(movieItem);
   console.log(movieItem);
@@ -50,7 +49,7 @@ function listMovies() {
     .then((response) => response.json())
     .then((data) => {
       for (let movie of data) {
-        console.log(movie, 'movie');
+        console.log(movie, "movie");
         createMovieBox(movie);
       }
     });
@@ -60,8 +59,8 @@ function listMovies() {
 function addMovie(movieText) {
   console.log(movieText);
   fetch(url, {
-    method: 'POST',
-    headers: { 'Content-type': 'application/json' },
+    method: "POST",
+    headers: { "Content-type": "application/json" },
     body: JSON.stringify({
       title: movieText,
       watched: false,
@@ -74,10 +73,25 @@ function addMovie(movieText) {
 
 // DELETE function
 
-function deleteMovie(element) {
+function deleteMovie(e) {
+  const movieId = e.parentElement.parentElement.id;
+  console.log(movieId);
+  fetch(url + "/" + `${movieId}`, {
+    method: "DELETE",
+  }).then(() => e.parentElement.parentElement.remove());
+}
+
+// UPDATE PUT function
+function updateMovieWatched(element) {
   const movieId = element.parentElement.parentElement.id;
-  console.log(movieId)
-  fetch(url + '/' + `${movieId}`, {
-    method: 'DELETE',
-  }).then(() => element.parentElement.parentElement.remove());
+  console.log(updateMovieWatched, "updateMovieWatched");
+  fetch(url + "/" + `${movieId}`, {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      watched_at: moment().format(),
+    }),
+  }).then();
+
+  // // need to finish promises
 }
